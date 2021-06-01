2fc27c31b24058508b2c88fdad45c29004174828
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-06-01 16:51:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="startpage-parser",
     version='1.0.0',
-    description='parsing search results from startpage search engine (startpage.com)',
+    description='parsing search results from startpage search engine (based on google.com results)',
     py_modules=["startpage"],
     package_dir={'':'src'},
     url='https://github.com/knassar702/startpage-parser',
