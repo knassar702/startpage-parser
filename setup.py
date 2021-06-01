68f5bb1782b5f90e1e06c9a0c248e6d4c81d2882
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-06-01 17:24:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name="startpage-parser",
     version='1.0.0',
+    license="GPL-3.0 License",
     description='parsing search results from startpage search engine (based on google.com results)',
     py_modules=["startpage"],
+    long_description=readme,
     package_dir={'':'src'},
     url='https://github.com/knassar702/startpage-parser',
     python_requires='>=3.6',
